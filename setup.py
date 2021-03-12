1f86af2b622c8436e300f738b1752b871e57157b
iaguilar <iaguilar@iaguilar.diphuelva.es>
2021-03-12 12:05:19 +0100

    Initial steps to publish on PyPi

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'PyMagicSwitchbot',
+    packages = ['magicswitchbot'],
+    install_requires=['bluepy'],
+    version = '0.0.1',
+    description = 'Library to control MagicSwitchbot devices (chinese clone of SwitchBot)',
+    author='Isidoro Aguilar Baeza',
+    url='https://github.com/ec-blaster/pyMagicSwitchbot',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
